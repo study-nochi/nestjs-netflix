60d36a7dc5290a31ab9dda71c8179b8b58c7a61f
ainochi-kor <kthrkdals@gmail.com>
2024-09-15 01:36:00 +0900

    GET movie 구현하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,29 @@
 import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Movie {
+  id: number;
+  title: string;
+}
+
 @Controller('movie')
 export class AppController {
+  #movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies() {
-    return [
-      {
-        id: 1,
-        name: '해리포터',
-        character: ['해리', '헤르미온느', '론'],
-      },
-      {
-        id: 2,
-        name: '반지의 제왕',
-        character: ['프로도', '사우론', '간다르'],
-      },
-    ];
+    return this.#movies;
   }
 
   @Get(':id')
