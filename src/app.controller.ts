edfacd5b52fb3ff7c7e3ccda3d6dead8f2498648
ainochi-kor <kthrkdals@gmail.com>
2024-09-15 01:33:46 +0900

    Movie API 설계 및 구현하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('movie')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMovies() {
+    return [
+      {
+        id: 1,
+        name: '해리포터',
+        character: ['해리', '헤르미온느', '론'],
+      },
+      {
+        id: 2,
+        name: '반지의 제왕',
+        character: ['프로도', '사우론', '간다르'],
+      },
+    ];
+  }
+
+  @Get(':id')
+  getMovie() {
+    return {
+      id: 1,
+      name: '해리포터',
+      character: ['해리', '헤르미온느', '론'],
+    };
+  }
+
+  @Post()
+  postMovie() {
+    return {
+      id: 3,
+      name: '어벤져스',
+      character: ['아이언맨', '토르', '헐크'],
+    };
+  }
+
+  @Patch(':id')
+  patchMovie() {
+    return {
+      id: 3,
+      name: '어벤져스',
+      character: ['아이언맨', '블랙위도우', '헐크'],
+    };
+  }
+
+  @Delete(':id')
+  deleteMovie() {
+    return 3;
   }
 }
